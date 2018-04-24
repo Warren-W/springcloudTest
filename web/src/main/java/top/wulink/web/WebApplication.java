99871b00bf1ef35f6b34ae8c39d4d259669a9e44
Warren <568082260@qq.com>
2018-04-24 18:21:07 +0800

    添加feign扫描路径

--- web/src/main/java/top/wulink/web/WebApplication.java
+++ web/src/main/java/top/wulink/web/WebApplication.java
@@ -6,12 +6,10 @@ import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
-import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-@EnableFeignClients
-@ComponentScan()
+@EnableFeignClients(basePackages = ("top.wulink"))
 public class WebApplication extends SpringBootServletInitializer{
 
     @Override
