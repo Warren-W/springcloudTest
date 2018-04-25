cb302aed574b48d1469abf1eebf3a8fe3d6c3e99
Warren <568082260@qq.com>
2018-04-25 16:36:04 +0800

    添加eureka集群

--- admin/src/main/java/top/wulink/admin/admin/LoginServer.java
+++ admin/src/main/java/top/wulink/admin/admin/LoginServer.java
@@ -1,16 +1,16 @@
 package top.wulink.admin.admin;
 
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import top.wulink.common.admin.api.LoginClient;
 import top.wulink.common.admin.bo.LoginBo;
 import top.wulink.common.admin.vo.LoginVo;
 
 
 @RestController
-public class LoginServer {
+public class LoginServer implements LoginClient{
 
-    @RequestMapping("login")
+    @Override
     public LoginVo loginDo(@RequestBody LoginBo bo) {
         LoginVo vo = new LoginVo();
         vo.setId(1);
